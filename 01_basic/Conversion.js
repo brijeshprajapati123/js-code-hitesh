e628d621fd3050734f5d359ed77137f62011379d
brijeshprajapati123 <122994569+brijeshprajapati123@users.noreply.github.com>
2023-09-25 10:44:15 +0000

    commit

---
+++ 01_basic/Conversion.js
@@ -0,0 +1,29 @@
+let score="33abc"
+console.log(typeof(score));
+let valuInNumber=Number(score)
+console.log(typeof valuInNumber);
+console.log(valuInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1; ""=>false,0=>false,
+// Return type of variables in JavaScript
+
+
+
+
+
+
+// 1) Primitive Datatypes
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
--- 01_basic/test.js
+++
@@ -1,1 +0,0 @@
-console.log("Hii,How are you Brijesh");
