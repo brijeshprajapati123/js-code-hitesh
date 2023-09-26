ebd2d5e03ef1944889df16e697c717e75a77227e
brijeshprajapati123 <122994569+brijeshprajapati123@users.noreply.github.com>
2023-09-26 02:53:02 +0000

    Commit

---
+++ 01_basic/Date.js
@@ -0,0 +1,21 @@
+// Dates
+
+// let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate=new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleDateString());
+// let myTimespend=Date.now()
+// console.log(myTimespend);
+// console.log(myCreatedDate.getTime());
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1)
+
+
